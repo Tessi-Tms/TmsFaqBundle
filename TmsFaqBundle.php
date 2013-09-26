a9897ce77a1ff4baf948b55e75724bd821d9ecbb
KayumbiHodiebDanielle <hodieb.danielle@yahoo.fr>
2013-09-26 10:06:50 +0200

    add TmsFaqBundle Class

---
+++ TmsFaqBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 
+ * @author:  TESSI Marketing <contact@tessi.fr>
+ *
+ */
+
+namespace Tms\Bundle\FaqBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class TmsFaqBundle extends Bundle
+{
+}
