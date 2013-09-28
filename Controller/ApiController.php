86bb0f7e7e9588b894375b948cad107bfa5dfb27
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2013-09-28 14:36:44 +0200

    Add Api controller

---
+++ Controller/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tms\Bundle\FaqBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Api controller.
+ */
+class ApiController extends Controller
+{
+}
