2b9985a8f71abf958bf40a42defe512a42ddf671
KayumbiHodiebDanielle <hodieb.danielle@yahoo.fr>
2013-10-04 09:28:30 +0200

    faq web service

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -3,13 +3,43 @@
 namespace Tms\Bundle\FaqBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Tms\Bundle\FaqBundle\Entity\Faq;
 
 /**
  * Api controller.
+ *
+ * @Route("/faqs")
  */
 class ApiController extends Controller
 {
+
+   /**
+    * Get a faq by its id
+    *
+    * @Route("/{id}.{_format}", name="tms_faq_api_faqs_get", defaults={"_format"="json"})
+    * @ParamConverter("faq", class="TmsFaqBundle:Faq", options={"id" = "id"})
+    * @Method("GET")
+    */
+    public function getAction(Request $request, Faq $faq)
+    {
+        $format = $request->getRequestFormat();
+        //$question = $this->get('tms_faq.manager.faq')->findBy(array('faq'=>$faq));
+        //$faq = $this->get('tms_faq.manager.faq')->findAllIn($faq->getId());
+        //var_dump($faq);die;
+        $export = $this->get('idci_exporter.manager')->export(array($faq), $format);
+
+        $response = new Response();
+        $response->setContent($export->getContent());
+        $response->headers->set(
+            'Content-Type',
+            sprintf('%s; charset=UTF-8', $export->getContentType())
+        );
+        return $response;
+    }
 }
