54353461b5ce84c2fe31e7ab3e49297e7c4efd27
PichetPuth <pichet.puth@gmail.com>
2014-07-18 17:33:38 +0200

    Work on controllers using TmsRestBundle and serializer

---
+++ Controller/Rest/ApiQuestionController.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ *
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @author:  Pichet PUTH <pichet.puth@utt.fr>
+ * @license: GPL
+ *
+ */
+
+namespace Tms\Bundle\FaqBundle\Controller\Rest;
+
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Util\Codes;
+use JMS\Serializer\SerializationContext;
+use Tms\Bundle\RestBundle\Formatter\AbstractHypermediaFormatter;
+
+/**
+ * Question API REST controller
+ */
+class ApiQuestionController extends FOSRestController
+{
+    /**
+     * [GET] /questions
+     * Retrieve a set of Question
+     *
+     * @QueryParam(name="limit", requirements="\d+", strict=true, nullable=true, description="(optional) Pagination limit")
+     * @QueryParam(name="offset", requirements="\d+", strict=true, nullable=true, description="(optional) Pagination offset")
+     * @QueryParam(name="page", requirements="\d+", strict=true, nullable=true, description="(optional) Page number")
+     * @QueryParam(name="sort", array=true, nullable=true, description="(optional) Sort")
+     *
+     * @param integer $limit
+     * @param integer $offset
+     * @param integer $page
+     * @param array   $sort
+     */
+    public function getQuestionsAction(
+        $limit      = null,
+        $offset     = null,
+        $page       = null,
+        $sort       = null
+    )
+    {
+        $view = $this->view(
+            $this
+                ->get('tms_rest.formatter.factory')
+                ->create(
+                    'orm_collection',
+                    $this->getRequest()->get('_route'),
+                    $this->getRequest()->getRequestFormat()
+                )
+                ->setObjectManager(
+                    $this->get('doctrine.orm.entity_manager'),
+                    $this
+                        ->get('tms_faq.manager.question')
+                        ->getEntityClass()
+                )
+                ->setCriteria(array())
+                ->setSort($sort)
+                ->setLimit($limit)
+                ->setOffset($offset)
+                ->setPage($page)
+                ->format()
+            ,
+            Codes::HTTP_OK
+        );
+
+        $serializationContext = SerializationContext::create()
+            ->setGroups(array(
+                AbstractHypermediaFormatter::SERIALIZER_CONTEXT_GROUP_COLLECTION
+            ))
+        ;
+        $view->setSerializationContext($serializationContext);
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * [GET] /questions/{id}
+     * Retrieve an Question
+     *
+     * @param integer $id
+     */
+    public function getQuestionAction($id)
+    {
+        try {
+            $view = $this->view(
+            $this
+                ->get('tms_rest.formatter.factory')
+                ->create(
+                    'item',
+                    $this->getRequest()->get('_route'),
+                    $this->getRequest()->getRequestFormat(),
+                    $id
+                )
+                ->setObjectManager(
+                    $this->get('doctrine.orm.entity_manager'),
+                    $this
+                        ->get('tms_faq.manager.question')
+                        ->getEntityClass()
+                )
+                ->format(),
+                Codes::HTTP_OK
+            );
+
+            $serializationContext = SerializationContext::create()
+                ->setGroups(array(
+                    AbstractHypermediaFormatter::SERIALIZER_CONTEXT_GROUP_ITEM
+                ))
+            ;
+            $view->setSerializationContext($serializationContext);
+
+            return $this->handleView($view);
+
+        } catch(NotFoundHttpException $e) {
+            return $this->handleView($this->view(
+                array(),
+                $e->getStatusCode()
+            ));
+        }
+    }
+}
