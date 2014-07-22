80afc40b62a2f0d7aada816327b22679d83b693e
PichetPuth <pichet.puth@gmail.com>
2014-07-22 18:47:43 +0200

    Add repositories

---
+++ Entity/Repository/FaqRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *
+ *@author: Danielle HODIEB <danielle.hodieb@tessi.fr>
+ *
+ */
+
+namespace Tms\Bundle\FaqBundle\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+
+/**
+ * FaqRepository
+ */
+class FaqRepository extends AbstractEntityRepository
+{
+}
---
+++ Entity/Repository/QuestionCategoryRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *
+ *@author: Pichet PUTH <pichet.puth@utt.fr>
+ *
+ */
+
+namespace Tms\Bundle\FaqBundle\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * QuestionCategoryRepository
+ */
+class QuestionCategoryRepository extends AbstractEntityRepository
+{
+}
