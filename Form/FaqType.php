8d141ad4c15fb4bd36ddeef85e1e306f1e49ef6d
PichetPuth <pichet.puth@gmail.com>
2014-07-28 17:21:54 +0200

    Rework FaqType : add hidden fields

--- Form/FaqType.php
+++ Form/FaqType.php
@@ -5,30 +5,67 @@ namespace Tms\Bundle\FaqBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Tms\Bundle\FaqBundle\Manager\FaqManager;
 
 class FaqType extends AbstractType
 {
+    protected $faqManager;
+
+    /**
+     * Constructor
+     *
+     * @param FaqManager $faqManager
+     */
+    public function __construct(FaqManager $faqManager)
+    {
+        $this->faqManager = $faqManager;
+    }
+
+    /**
+     * Get faq manager
+     *
+     * @return FaqManager
+     */
+    public function getFaqManager()
+    {
+        return $this->faqManager;
+    }
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $object = $options['object'];
+
         $builder
             ->add('title')
             ->add('enabled', 'checkbox', array('required' => false))
-            ->add('customerId')
+            ->add('objectClassName', 'hidden', array(
+                'data' => $this->getFaqManager()->getClassName($object)
+            ))
+            ->add('objectId', 'hidden', array(
+                'data' => $object->getId()
+            ))
+            ->add('hash', 'hidden', array(
+                'data' => $this->getFaqManager()->generateHash($object)
+            ))
         ;
     }
-    
+
     /**
      * @param OptionsResolverInterface $resolver
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults(array(
-            'data_class' => 'Tms\Bundle\FaqBundle\Entity\Faq'
-        ));
+        $resolver
+            ->setDefaults(array(
+                'data_class' => 'Tms\Bundle\FaqBundle\Entity\Faq',
+            ))
+            ->setRequired(array('object'))
+            ->setAllowedTypes(array('object' => 'Tms\Bundle\FaqBundle\FaqOwnerInterface'))
+        ;
     }
 
     /**
