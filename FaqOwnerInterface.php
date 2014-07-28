8ee35e771638dabac6ba1d9399659e668285559c
PichetPuth <pichet.puth@gmail.com>
2014-07-28 17:23:43 +0200

    Add FaqOwnerInterface

---
+++ FaqOwnerInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author Pichet PUTh <pichet.puth@utt.fr>
+ */
+
+namespace Tms\Bundle\FaqBundle;
+
+interface FaqOwnerInterface
+{
+    /**
+     * Get id
+     *
+     * @return string
+     */
+    public function getId();
+}
