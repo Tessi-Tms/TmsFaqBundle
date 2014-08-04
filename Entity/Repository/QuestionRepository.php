ae8a7c29526461db1581256bef34549c695e5443
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2014-08-04 13:42:12 +0200

    Move the AbstractEntityRepository to TmsRestBundle

--- Entity/Repository/QuestionRepository.php
+++ Entity/Repository/QuestionRepository.php
@@ -2,14 +2,15 @@
 
 /**
  *
- *@author: Danielle HODIEB <danielle.hodieb@tessi.fr>
+ * @author:  Danielle HODIEB <danielle.hodieb@tessi.fr>
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @license: MIT
  *
  */
 
 namespace Tms\Bundle\FaqBundle\Entity\Repository;
 
-use Doctrine\ORM\EntityRepository;
-
+use Tms\Bundle\RestBundle\Repository\AbstractEntityRepository;
 
 /**
  * QuestionRepository
